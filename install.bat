c89dc4b96bfb6e04f7f2b9eb1b39939edb8fd028
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-30 14:24:52 +0200

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set install=%CD%\jarvis-pyrevit
+set repo=https://github.com/revitron/pyRevit.git
+set jarvis=https://github.com/revitron/jarvis.git
+set jarvisUI=https://github.com/revitron/jarvis-ui.git
+set revitron=https://github.com/revitron/revitron.git
+set revitronUi=https://github.com/revitron/revitron-ui.git
+set extensions=%install%\extensions
+
+if exist %install% (
+    del /s /q /f "%install%\.git\*"
+    rmdir /s /q "%install%\.git"
+    del /s /q /f "%install%\*"
+    rmdir /s /q "%install%"
+)
+
+timeout 3
+
+git clone %repo% %install%
+
+echo Installing core extensions ...
+%install%\bin\pyrevit extend lib jarvis %jarvis% --dest=%extensions%
+%install%\bin\pyrevit extend ui jarvis-ui %jarvisUI% --dest=%extensions%
+%install%\bin\pyrevit extend lib revitron %revitron% --dest=%extensions%
+%install%\bin\pyrevit extend ui revitron-ui %revitronUi% --dest=%extensions%
+
+cd /D %pwd%
